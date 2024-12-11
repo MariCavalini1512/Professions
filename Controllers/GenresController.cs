83cdd5edc4966a95885f946285f35e6047a75e50
MariCavalini1512 <maryadrielle@hotmail.com>
2024-12-11 08:02:06 -0300

    feat: Add base Project

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Professions.Models;
+
+namespace Professions.Controllers
+{
+    public class GenresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>()
+
+            {
+            new Genre
+                {
+                    Id = 1,
+                    Name = "Professor"
+                },
+            new Genre
+
+                {
+                    Id = 2,
+                    Name = "Fisioterapeuta"
+                },
+
+            new Genre
+
+                {
+                    Id = 3,
+                    Name = "Engenheiro"
+                }
+            };
+            
+            
+            return View(genres);
+        }
+    }
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Professions.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Display (Name = "Nome")]
+        public string Name { get; set; }
+        public string NivelEscolaridade { get; set; }
+        public string MediaSalarial { get; set; }
+        public string AreaAtuacao { get; set; }
+
+
+        // public ICollection<Nome> Nomes { get; set; } = new List<Nome>();
+
+        public Genre() { }
+
+        public Genre(int Id, string Nome) 
+        
+        { 
+        
+        }
+
+
+    }
+}
